23cf5e089c7f7020e84078ca54dc721a77d88493
TheBlackElder <freedxcollective@gmail.com>
2023-02-22 18:15:22 -0500

    props and state for form

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,30 +2,26 @@ import React from "react";
 
 import InterviewerListItem from "./InterviewerListItem";
 
-import "components/InterviewerList.scss"; 
-
-
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
- 
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map((interviewer) => {
     console.log(interviewer);
     return (
-    
       <InterviewerListItem
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.value}
-      setInterviewer={() => props.onChange(interviewer.id)}
-      />     
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      />
     );
   });
 
-  return <section className="interviewers">
+  return (
+    <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{interviewers}</ul>
     </section>
+  );
 }
-
-
